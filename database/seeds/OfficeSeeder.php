0c5ab759f871bd37970a709fb03d28357c344081
Shane Richards <shane.richards121@gmail.com>
2017-06-01 12:59:26 -0500

    commmented incompatible sql statements

--- database/seeds/OfficeSeeder.php
+++ database/seeds/OfficeSeeder.php
@@ -18,7 +18,7 @@ class OfficeSeeder extends Seeder
 
 		//disable foreign key check for this connection before running seeders
 		
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         
         DB::table('churches')->truncate();
         DB::table('district_offices')->truncate();
@@ -30,7 +30,7 @@ class OfficeSeeder extends Seeder
         
 		// supposed to only apply to a single connection and reset it's self
 		// but I like to explicitly undo what I've done for clarity
-		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+		// DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         
         $nationalOffice = NationalOffice::create(['name' => 'Baptist Union National Office']);
     
